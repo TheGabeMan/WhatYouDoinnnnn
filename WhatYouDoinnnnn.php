80dc9da6be6825fd444decee77a1839c1e8da396
TheGabeMan <TheGabeMan@gmail.com>
2017-07-02 00:55:24 +0200

    Adding MySQL DB connection

--- WhatYouDoinnnnn.php
+++ WhatYouDoinnnnn.php
@@ -109,10 +109,8 @@ function WriteRow($chatId, $message){
      * Activity Type = Type of activity which can be IPWC (Incident, Problem, Wijziging, Controle) or D (Desk Meeting) or M (Meeting) or R (pRoject)
      * 
      */
-}
 
-require_once("basic-functions.php");
-require("open-database.php");
+require_once("open-database.php");
 
 $date = date("Y-m-d H:i:s");
 
@@ -124,5 +122,25 @@ $sqlString = "INSERT INTO `ActivTable`(`chatid`,`timestamp`,starttime`,`duration
         
       }
     $results = mysqli_query($dbcon,$sql);
+}
+
+function WriteToLogDB($URLOKArray)
+{
+    require_once( 'open-database.php');
+    
+    // Write to log table
+    $sql = "INSERT INTO `ConnectionLog`(`timestamp`,`url01`,`url02`, `url03`) VALUES( now(), $URLOKArray[0], $URLOKArray[1],$URLOKArray[2]);";
+    printf($sql);
+    if (!mysqli_query($dbcon,$sql))
+      { die('Error: ' . mysqli_error($dbcon)); }
+    $results = mysqli_query($dbcon,$sql);
+    
+    
+    printf("%d Row inserted.\n", mysqli_affected_rows($dbcon));
+    printf("Error Nr.\n", mysqli_errno($dbcon));
+    printf("Error  \n",  mysqli_error($dbcon));
+    
+    mysqli_close($dbcon);
+}
 
 ?>
